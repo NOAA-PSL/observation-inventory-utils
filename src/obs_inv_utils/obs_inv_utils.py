f872ca3bca5e4b91a06c5615f1dcf8216bb3e702
Steve Lawrence <steve.lawrence@noaa.gov>
2022-01-13 13:35:14 -0700

    feat: add unit testing capability with pytest and tox

--- src/obs_inv_utils/obs_inv_utils.py
+++ src/obs_inv_utils/obs_inv_utils.py
@@ -1,4 +1,32 @@
+"""
+Copyright 2022 NOAA
+All rights reserved.
+
+Collection of methods to inventory observation files
+
+"""
 import click
+import logging
+import os
+from obs_inv_utils import io_utils
+
+logLevel = os.environ.get("LOG_LEVEL")
+if logLevel is None:
+    logLevel = logging.DEBUG
+logger = logging.getLogger(__name__)
+try:
+    logging.basicConfig(
+        format='%(asctime)s %(levelname)-8s %(message)s',
+        level=logLevel,
+        datefmt='%Y-%m-%d %H:%M:%S'
+    )
+except Exception:
+    logger.exception('bad log level env var')
+    logging.basicConfig(
+        format='%(asctime)s %(levelname)-8s %(message)s',
+        level=logging.INFO,
+        datefmt='%Y-%m-%d %H:%M:%S'
+    )
 
 @click.group()
 def cli():
@@ -7,4 +35,3 @@ def cli():
 @cli.command()
 def hello_world():
     click.echo('Hello World!')
-    
