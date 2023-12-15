121e40074ce19385662e0024cc7249c92ab9f8d3
Jessica Knezha <jessica.knezha@noaa.gov>
2023-12-15 11:59:29 -0700

    update n0# sats list

--- src/misc/plot_mysql_sensor_sat.py
+++ src/misc/plot_mysql_sensor_sat.py
@@ -24,17 +24,17 @@ args = parser.parse_args()
 satinfo_db_root=args.satinfo_db_root
 daterange=[date(1975,1,1), date(2024,1,1)]
 
-sat_dictionary={"NOAA 5": "n5", "NOAA 6": "n6", "NOAA 7": "n7", "NOAA 8": "n8", "NOAA 9": "n9", 
+sat_dictionary={"NOAA 5": "n05", "NOAA 6": "n06", "NOAA 7": "n07", "NOAA 8": "n08", "NOAA 9": "n09", 
                 "NOAA 10":"n10", "NOAA 11":"n11","NOAA 12":"n12","NOAA 13":"n13","NOAA 14":"n14",
                "NOAA 15":"n15","NOAA 16":"n16","NOAA 17":"n17","NOAA 18":"n18","NOAA 19":"n19","NOAA 20":"n20",
                "METOP-1":"metop-b","METOP-2":"metop-a","METOP-3":"metop-c",
                "METOP-1 (Metop-A":"metop-b","METOP-2 (Metop-B":"metop-a","METOP-3 (Metop-C":"metop-c",
                "METOP-1 (Metop-B":"metop-b","METOP-2 (Metop-A":"metop-a",
                "AQUA":"aqua", "NPP":"npp",
-               "GOES 7" : "g7", "GOES 8": "g08", "GOES 9": "g09", "GOES 10": "g10","GOES 11": "g11","GOES 12": "g12",
+               "GOES 7" : "g07", "GOES 8": "g08", "GOES 9": "g09", "GOES 10": "g10","GOES 11": "g11","GOES 12": "g12",
                "GOES 13": "g13", "GOES 14": "g14", "GOES 15": "g15", "GOES 16" : "g16", "GOES 17":"g17",
                "MTSAT-2":"MTSAT-2", "MTSAT-1R":"MTSAT-1R",
-               "METEOSAT 2" : "m02", "METEOSAT 3": "m03", "METEOSAT 4": "m04","METEOSAT 5": "m05", "METEOSAT 6": "m06", "METEOSAT 7": "m7",
+               "METEOSAT 2" : "m02", "METEOSAT 3": "m03", "METEOSAT 4": "m04","METEOSAT 5": "m05", "METEOSAT 6": "m06", "METEOSAT 7": "m07",
                "METEOSAT 8": "m08", "METEOSAT 9": "m09", "METEOSAT 10": "m10", "METEOSAT 11": "m11",
                "":"",
                "DMSP 8":"f8", "DMSP 9":"f9", "DMSP 10": "f10", "DMSP 11": "f11", "DMSP 12": "f12", "DMSP 13": "f13", 
