0e4959a245852cadaabb9dac87aa1cec9e094ebb
Jessica Knezha <jessica.knezha@noaa.gov>
2024-01-09 15:36:34 -0700

    single time setting function

--- src/automation/auto_inventory.py
+++ src/automation/auto_inventory.py
@@ -4,8 +4,8 @@ import yaml_generation as yg
 import obs_inv_utils.obs_inv_cli as cli
 
 import argparse
-import subprocess
 from joblib import Parallel, delayed
+import os
 
 from datetime import datetime as dt
 from datetime import timedelta
@@ -14,11 +14,9 @@ from pandas import Timestamp
 #argparse section
 parser = argparse.ArgumentParser()
 parser.add_argument("-cat", dest="category", help="Category of variables to inventory. Valid options: atmosphere", choices=['atmosphere'], default="atmosphere", type=str)
-#parser.add_argument("-p", dest="platform", help="Platform the script is being run on. Valid options: pw, hera", choices=['pw', 'hera'], default="pw", type=str)
 parser.add_argument("-end", dest="end_date", help="End date to use for run. Format expected %Y%m%dT%H%M%SZ. If not provided, uses the current time.", type=str)
 parser.add_argument("-ago", dest="days_ago", help="Number of days ago to run the inventory for. If provided, must be positive integer. If not provided, it will run the full extent of the inventory.", default=0, type=int)
 args = parser.parse_args()
-#ADD AN ARGUMENT OPTION FOR END DATE IN CASE WANT TO NOT RUN TO NOW / DAYS AGO WILL BE BASED OFF THIS END DATE 
 
 #get category list
 # more categories to be added as they are written as dictionaries 
@@ -27,35 +25,31 @@ to_inventory = []
 if args.category == 'atmosphere':
     to_inventory = atm_dicts.atm_infos 
 
-#need to run correct sh for platform 
-# assume this is gonna be done before running the script because I had to do it to run initally
-# and don't want to give permission to run these files from this script 
-# if args.platform is 'pw':
-#     subprocess.run(['./../../obs_inv_utils_pw_cloud.sh'])
-# elif args.platform is 'hera':
-#     subprocess.run(['./../../obs_inv_utils_hera.sh'])
-
-#set end date time
-if len(args.end_date) > 0:
-    end_date = dt.strptime(args.end_date, au.DATESTR_FORMAT)
-    end = Timestamp(end_date).round(freq='6H')
-else:
-    end = Timestamp.now().round(freq='6H')
-
-print(f'End date: {end}')
+#set end date time and put in 6H time 
+# if len(args.end_date) > 0:
+#     end_date = dt.strptime(args.end_date, au.DATESTR_FORMAT)
+#     end = Timestamp(end_date).round(freq='6H')
+# else:
+#     end = Timestamp.now().round(freq='6H')
     
 def get_start_end_time(inventory_info):
+    if inventory_info.cycling_interval == au.CYCLING_6H:
+        frequency = '6H'
+    else: #default to round to closest hour as I think that's the lowest current unit 
+        frequency = 'H'
+
+    if len(args.end_date) > 0:
+        end_date = dt.strptime(args.end_date, au.DATESTR_FORMAT)
+        end = Timestamp(end_date).round(freq=frequency)
+    else:
+        end = Timestamp.now().round(freq=frequency)
+
     if args.days_ago > 0:
-        #end = Timestamp.now().round(freq='6H')
-        #end = dt.now() # need to get in 0/6/12/18z most recent value 
         end_time = end.strftime(au.DATESTR_FORMAT)
         start = end - timedelta(days=args.days_ago)
         start_time = start.strftime(au.DATESTR_FORMAT)
-        print(f'Start time: {start_time}, End time: {end_time}')
-    else: #run the full period from start to now
+    else: #run the full period from start to now if no days ago
         start_time = inventory_info.start
-        #end = dt.now() # need to get this in the 0 / 6 / 12 / 18z most recent value 
-        #end = Timestamp.now().round(freq='6H')
         end_time = end.strftime(au.DATESTR_FORMAT)
 
     return start_time, end_time
@@ -63,44 +57,49 @@ def get_start_end_time(inventory_info):
 #define functions to run in parallel 
 def run_obs_inventory(inventory_info):
     #EXPAND THIS LATER TO HANDLE TWO DAY RUNS; probably specify end time as part fo the argument options above
-    if args.days_ago > 0:
-        #end = Timestamp.now().round(freq='6H')
-        #end = dt.now() # need to get in 0/6/12/18z most recent value 
-        end_time = end.strftime(au.DATESTR_FORMAT)
-        start = end - timedelta(days=args.days_ago)
-        start_time = start.strftime(au.DATESTR_FORMAT)
-        print(f'Start time: {start_time}, End time: {end_time}')
-    else: #run the full period from start to now
-        start_time = inventory_info.start
-        #end = dt.now() # need to get this in the 0 / 6 / 12 / 18z most recent value 
-        #end = Timestamp.now().round(freq='6H')
-        end_time = end.strftime(au.DATESTR_FORMAT)
+    # if args.days_ago > 0:
+    #     #end = Timestamp.now().round(freq='6H')
+    #     #end = dt.now() # need to get in 0/6/12/18z most recent value 
+    #     end_time = end.strftime(au.DATESTR_FORMAT)
+    #     start = end - timedelta(days=args.days_ago)
+    #     start_time = start.strftime(au.DATESTR_FORMAT)
+    #     print(f'Start time: {start_time}, End time: {end_time}')
+    # else: #run the full period from start to now
+    #     start_time = inventory_info.start
+    #     #end = dt.now() # need to get this in the 0 / 6 / 12 / 18z most recent value 
+    #     #end = Timestamp.now().round(freq='6H')
+    #     end_time = end.strftime(au.DATESTR_FORMAT)
+    start_time, end_time = get_start_end_time(inventory_info)
 
     yaml_file = yg.generate_obs_inv_config(inventory_info, start_time, end_time)
     cli.get_obs_inventory_base(yaml_file)
+    os.remove(yaml_file)
 
 def run_nceplibs(inventory_info):
-    #ADD TWO DAY EXPANSION HERE AS WELL
-    if args.days_ago > 0:
-        #end = Timestamp.now().round(freq='6H')
-        #end = dt.now() # need to get in 0/6/12/18z most recent value 
-        end_time = end.strftime(au.DATESTR_FORMAT)
-        start = end - timedelta(days=args.days_ago)
-        start_time = start.strftime(au.DATESTR_FORMAT)
-        print(f'Start time: {start_time}, End time: {end_time}')
-    else: #run the full period from start to now
-        start_time = inventory_info.start
-        #end = dt.now() # need to get this in the 0 / 6 / 12 / 18z most recent value 
-        #end = Timestamp.now().round(freq='6H')
-        end_time = end.strftime(au.DATESTR_FORMAT)
+    # if args.days_ago > 0:
+    #     #end = Timestamp.now().round(freq='6H')
+    #     #end = dt.now() # need to get in 0/6/12/18z most recent value 
+    #     end_time = end.strftime(au.DATESTR_FORMAT)
+    #     start = end - timedelta(days=args.days_ago)
+    #     start_time = start.strftime(au.DATESTR_FORMAT)
+    #     print(f'Start time: {start_time}, End time: {end_time}')
+    # else: #run the full period from start to now
+    #     start_time = inventory_info.start
+    #     #end = dt.now() # need to get this in the 0 / 6 / 12 / 18z most recent value 
+    #     #end = Timestamp.now().round(freq='6H')
+    #     end_time = end.strftime(au.DATESTR_FORMAT)
+
+    start_time, end_time = get_start_end_time(inventory_info)
     
     #run correct command
     if inventory_info.nceplibs_cmd == au.NCEPLIBS_SINV:
         yaml_file = yg.generate_nceplibs_sinv_inventory_config(inventory_info, start_time, end_time)
         cli.get_obs_count_meta_sinv_base(yaml_file)
+        os.remove(yaml_file)
     elif inventory_info.nceplibs_cmd == au.NCEPLIBS_CMPBQM:
         yaml_file = yg.generate_nceplibs_cmpbqm_inventory_config(inventory_info, start_time, end_time)
         cli.get_obs_count_meta_cmpbqm_base(yaml_file)
+        os.remove(yaml_file)
     else:
         print(f'No valid commmand found for nceplibs_cmd in {inventory_info.obs_name} inventory info with value: ' + inventory_info.nceplibs_cmd)
 
@@ -117,7 +116,5 @@ Parallel(n_jobs=10)(delayed(run_full_inventory)(info) for info in to_inventory)
 
 print('Auto inventory script completed for ')
 for i in to_inventory: print(i.obs_name)
-
-#NEED TO ADD A CLEAN UP OF THE YAML FILES SOMEWHERE IN HERE 
 
 #next step would be to trigger the plotting scripts again 
