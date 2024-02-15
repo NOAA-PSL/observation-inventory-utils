0bc8b2aa1db3817d04c9a4cd34042202ff00a952
Jessica Knezha <jessica.knezha@noaa.gov>
2024-02-15 13:14:00 -0700

    use variables

--- src/automation/run_3day_inventory.sh
+++ src/automation/run_3day_inventory.sh
@@ -3,17 +3,19 @@
 # the past 3 days to get updates for lagged data and plotting
 # the new data.
 
-SATINFO_LOC=/home/info/something_here
+SATINFO_DIR=/home/Jessica.Knezha/obs-inventory/build_gsinfo/satinfo
+OUTPUT_LOC=/home/Jessica.Knezha/inventory-figures
+WORK_DIR=/lustre/home/work/inventory-work
 
 source ../../obs_inv_utils_pw_cloud.sh
 
 #run inventory 
-python3 auto_inventory.py -cat atmosphere -ago 3 -n_jobs 40 -work_dir /lustre/home/work/inventory-work 
+python3 auto_inventory.py -cat atmosphere -ago 3 -n_jobs 40 -work_dir $WORK_DIR 
 
 #run all plots in parallel
-python3 ../plotting/plot_mysql_dir_sensor_sat.py --sidb $SATINFO_LOC -o /inventory_figures &
-python3 ../plotting/plot_mysql_sensor.py -o /inventory_figures &
-python3 ../plotting/plot_mysql_sensor_sat_amv.py --sidb $SATINFO_LOC -o /inventory_figures &
-python3 ../plotting/plot_mysql_sensor_sat_geo.py --sidb $SATINFO_LOC -o /inventory_figures &
-python3 ../plotting/plot_mysql_sensor_sat_gps.py --sidb $SATINFO_LOC -o /inventory_figures &
-python3 ../plotting/plot_mysql_typ.py -o /inventory_figures 
+python3 ../plotting/plot_mysql_dir_sensor_sat.py --sidb $SATINFO_DIR -o $OUTPUT_LOC &
+python3 ../plotting/plot_mysql_sensor.py -o $OUTPUT_LOC &
+python3 ../plotting/plot_mysql_sensor_sat_amv.py --sidb $SATINFO_DIR -o $OUTPUT_LOC &
+python3 ../plotting/plot_mysql_sensor_sat_geo.py --sidb $SATINFO_DIR -o $OUTPUT_LOC &
+python3 ../plotting/plot_mysql_sensor_sat_gps.py --sidb $SATINFO_DIR -o $OUTPUT_LOC &
+python3 ../plotting/plot_mysql_typ.py -o $OUTPUT_LOC 
