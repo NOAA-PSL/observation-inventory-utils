8d5cc81937720e815d7c15c286af70e0b7c6f3ff
Jessica Knezha <jessica.knezha@noaa.gov>
2024-02-16 17:32:08 -0700

    increase pool size

--- src/obs_inv_utils/inventory_table_factory.py
+++ src/obs_inv_utils/inventory_table_factory.py
@@ -9,6 +9,7 @@ from sqlalchemy import inspect
 from sqlalchemy.orm import relationship, backref
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.pool import NullPool
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -51,7 +52,7 @@ else:
     OBS_DATABASE = f"sqlite:///{sqlite_database}"
     print('sqlite database: ' + OBS_DATABASE)   
 
-engine = db.create_engine(OBS_DATABASE)
+engine = db.create_engine(OBS_DATABASE, poolclass=NullPool)
 Base = declarative_base()
 metadata = MetaData(engine)
 Session = sessionmaker(bind=engine)
