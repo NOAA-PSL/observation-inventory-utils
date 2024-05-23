351b8218e19275b82c50e857a94049d384ef4c34
Jessica Knezha <jessica.knezha@noaa.gov>
2024-05-23 10:30:26 -0400

    add pool recycle

--- src/obs_inv_utils/inventory_table_factory.py
+++ src/obs_inv_utils/inventory_table_factory.py
@@ -51,7 +51,7 @@ else:
     OBS_DATABASE = f"sqlite:///{sqlite_database}"
     print('sqlite database: ' + OBS_DATABASE)   
 
-engine = db.create_engine(OBS_DATABASE, pool_size=150, max_overflow=0)
+engine = db.create_engine(OBS_DATABASE, pool_size=150, max_overflow=0, pool_recycle=280)
 Base = declarative_base()
 metadata = MetaData(engine)
 Session = sessionmaker(bind=engine)
