30c1d4efb77ab42894d03f8631182f80b1b0fce6
Jessica Knezha <jessica.knezha@noaa.gov>
2024-09-05 16:50:22 -0600

    new files for configuring new cluster

---
+++ obs_inv_utils_pw_inv_cluster.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#
+# script for configuring set up to run on obs inventory cluster on parallel works
+
+
+module load gnu
+module load intel/2023.2.0
+module load impi/2023.2.0
+export PATH=/contrib/inv-stack/NCEPLIBS-bufr-12.1.0/utils:$PATH 
+
+OBS_INV_HOME_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+export PYTHONPATH=$OBS_INV_HOME_DIR/src
+echo PYTHONPATH=$PYTHONPATH
+echo Python Version: $(python --version)
+
