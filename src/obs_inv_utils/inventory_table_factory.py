c44c427fe8a989e69cc39f0090c940844f86814b
Jessica Knezha <jessica.knezha@noaa.gov>
2024-09-11 10:35:08 -0600

    update error handing

--- src/obs_inv_utils/inventory_table_factory.py
+++ src/obs_inv_utils/inventory_table_factory.py
@@ -640,11 +640,13 @@ def insert_obs_meta_nceplibs_prepbufr_item(obs_meta_items):
             (obs_id, cmd_result_id, cmd_str, variable, typ, tot, qm0thru3, qm4thru7, qm8, qm9, qm10, qm11, qm12, qm13, qm14, qm15, cka, ckb, filename, file_size, obs_day, inserted_at)
             VALUES (:obs_id, :cmd_result_id, :cmd_str, :variable, :typ, :tot, :qm0thru3, :qm4thru7, :qm8, :qm9, :qm10, :qm11, :qm12, :qm13, :qm14, :qm15, :cka, :ckb, :filename, :file_size, :obs_day, :inserted_at)
             """
-
-    session = Session()
-    session.execute(text(sql), rows)
-    session.commit()
-    session.close()
+    if len(rows) > 0:
+        session = Session()
+        session.execute(text(sql), rows)
+        session.commit()
+        session.close()
+    else:
+        print("No data provided to insert. No data inserted into the prepbfur meta table.")
 
 def insert_obs_meta_nceplibs_prepbufr_agg_item(obs_meta_items):
     if not isinstance(obs_meta_items, list):
