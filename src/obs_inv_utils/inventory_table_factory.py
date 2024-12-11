cd18d75dde3adb8908df4af2cc4cb8a209325f69
Jessica Knezha <jessica.knezha@noaa.gov>
2024-12-11 10:03:37 -0700

    add character length

--- src/obs_inv_utils/inventory_table_factory.py
+++ src/obs_inv_utils/inventory_table_factory.py
@@ -575,7 +575,7 @@ class ObsMetaHvIodaNetcdf(Base):
     sensor = Column(String(63))
     platform = Column(String(128))
     ioda_layout = Column(String(63))
-    processing_level = Column(String())
+    processing_level = Column(String(63))
     thinning = Column(Float())
     ioda_version = Column(String(63))
     filename = Column(String(63))
