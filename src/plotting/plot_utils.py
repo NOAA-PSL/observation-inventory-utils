b7e19cdc891f970c2d7630602366e0bd99e4e774
Jessica Knezha <jessica.knezha@noaa.gov>
2025-03-20 13:31:23 -0600

    it was in the wrong place

--- src/plotting/plot_utils.py
+++ src/plotting/plot_utils.py
@@ -362,7 +362,7 @@ def get_wod_nc():
         omwn.obs_day
     ).join(
         oi,
-        omwn.obs_id == oi.obs_ids
+        omwn.obs_id == oi.obs_id
     ).filter(
         oi.s3_bucket == 'noaa-reanalyses-pds'
     )
