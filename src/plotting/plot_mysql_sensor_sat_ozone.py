9d27d0f8b4b7277dd6d421da6dfe6385fec60f90
Jessica Knezha <jessica.knezha@noaa.gov>
2025-05-08 16:39:54 -0600

    remove excess print

--- src/plotting/plot_mysql_sensor_sat_ozone.py
+++ src/plotting/plot_mysql_sensor_sat_ozone.py
@@ -98,13 +98,11 @@ counter=0
 for index, row in unique_sensor_sats.iterrows():
     try:
         satinfo_string_ = row['subsensor']+"_"+ utils.sat_dictionary[row['sat_id_name']]
-        print(satinfo_string_)
     except KeyError as err:
         print(f'unable to get satinfo string for row: {row}')
         print(f'Error: {err}')
         satinfo_string_ = row['subsensor']
     satinfo = utils.read_ozinfo_files(satinfo_db_root,satinfo_string_)
-    print(f'satinfo for {satinfo_string_}: {satinfo}')
 
     pandas.options.mode.chained_assignment = None
     dftmp = select_subsensor_satellite_combo(row['subsensor'], row['sat_id'], db_frame, satinfo)
