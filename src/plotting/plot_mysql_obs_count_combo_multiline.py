fb59c1ac80af3658975670aae866ed98e2c0af9a
Jessica Knezha <jessica.knezha@noaa.gov>
2025-06-17 18:18:31 -0600

    size

--- src/plotting/plot_mysql_obs_count_combo_multiline.py
+++ src/plotting/plot_mysql_obs_count_combo_multiline.py
@@ -100,7 +100,7 @@ grouped_df['rolling_avg'] = grouped_df.groupby('category')['obs_count'].transfor
 unique_categories = grouped_df['category'].unique()
 
 # Create the plot
-fig, ax = plt.subplots(figsize=(10, 6))  # Increase figure width
+fig, ax = plt.subplots(figsize=(10, 4))  # Increase figure width
 
 for category in unique_categories:
     single_category_df = grouped_df[(grouped_df['category'] == category)]
