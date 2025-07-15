95c3a414f80352cb42bc2c89b632e17ec2d0f66c
Jessica Knezha <jessica.knezha@noaa.gov>
2025-07-15 12:07:30 -0600

    another font size try

--- src/plotting/plot_mysql_obs_count_combo_multiline.py
+++ src/plotting/plot_mysql_obs_count_combo_multiline.py
@@ -115,7 +115,7 @@ for category in unique_categories:
 
     ax.plot(single_category_df['date_only'], single_category_df['rolling_avg'], label=f'{category_titles[category]}')
 
-ax.set_title(f'{args.title}', fontsize = 19) #16
+ax.set_title(f'{args.title}', fontsize = 18) #16
 ax.set_xlabel('Observation Day', fontsize = 17) #14
 ax.set_ylabel('Average Daily Observation Count', fontsize = 17) #14
 ax.set_yscale('log')  # log10 y-axis
@@ -129,7 +129,7 @@ ax.tick_params(labelsize=12)
 
 # Add grid and legend
 ax.grid(True)
-ax.legend(fontsize = 13) #11
+ax.legend(fontsize = 12) #11
 
 plt.tight_layout()
 # plt.suptitle(f'accurate as of {datetime.now().strftime("%m/%d/%Y %H:%M:%S")} UTC', y=-0.01)
