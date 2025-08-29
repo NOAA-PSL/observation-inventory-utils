267901f6bed5fe20ac59d6d50cbf451a31d56120
Jessica Knezha <jessica.knezha@noaa.gov>
2025-08-29 14:48:29 -0600

    add new summation plot

---
+++ src/plotting/plot_mysql_obs_count_combo_sums.py
@@ -0,0 +1,150 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+from datetime import datetime, date
+import matplotlib.dates as mdates
+import os
+import argparse
+import plot_utils as utils
+import obs_inv_utils.inventory_table_factory as itf
+
+#argparse section
+parser = argparse.ArgumentParser()
+parser.add_argument("-o", dest='out_dir', help="output directory for figures",default='figures',type=str)
+parser.add_argument("-dev", dest='dev', help='Use this flag to add a timestamp to the filename for development', default=False, type=bool)
+parser.add_argument("-window", dest='window', help=" Rolling average of window size", type=int, default=1)
+parser.add_argument("-title", dest='title', help='Title for the plot', type=str, default="Time Series of Observation Count")
+parser.add_argument("-cats", dest='cat_list', help="Categories of sensors to plot", type=str, nargs='+')
+args = parser.parse_args()
+
+category_dicts = {
+    'AMV': {'amv'},
+    'GPS': {'gps'},
+    'geo_rad' : {'geo/ahicsr', 'geo/geoimr', 'geo/goesfv', 'geo/goesnd', 'geo/gsrasr'},
+    'hyper_infrared': {'cris', 'iasi', 'airs/nasa'}, 
+    'multi_infrared': {'ssu', 'hirs/1bhrs2', 'hirs/1bhrs3', 'hirs/1bhrs4'}, 
+    'micro_imagers': {'gmi', 'amsr2', 'tmi', 'amsre', 'ssmi', 'ssmis'},
+    'micro_sounders': {'saphir', 'mhs', 'atms', 'msu', 'amsub', 'amsua/1bamua'}, 
+    'ozone': {'ozone'},
+    'polar_orbit_BT': {'cris', 'iasi', 'airs/nasa', 'ssu', 'hirs/1bhrs2', 'hirs/1bhrs3', 'hirs/1bhrs4' , 'gmi', 'amsr2', 'tmi', 'amsre', 'ssmi', 'ssmis', 'saphir', 'mhs', 'atms', 'msu', 'amsub', 'amsua/1bamua'},
+    'tovs': {'hirs/1bhrs2', 'ssu', 'msu'},
+    'atovs': {'hirs/1bhrs3', 'hirs/1bhrs4', 'amsua/1bamua', 'amsub', 'mhs'},
+    'post-atovs': {'cris', 'atms', 'iasi'}
+}
+
+category_titles = {
+    'AMV': 'AMV',
+    'GPS': 'GPS',
+    'geo_rad': 'Geostationary Radiances',
+    'hyper_infrared': 'Hyperspectral Infrared',
+    'multi_infrared': 'Multispectral Infrared',
+    'micro_imagers': 'Microwave Imagers', 
+    'micro_sounders': 'Microwave Sounders', 
+    'ozone': 'Ozone',
+    'polar_orbit_BT': 'Polar Orbiting Brightness Temperature',
+    'tovs': 'TOVS',
+    'atovs': 'ATOVS',
+    'post-atovs': 'Post-ATOVS',
+}
+
+
+#parameters
+daterange=[date(1979,1,1), date(2026,1,1)]
+
+def select_sensor(sensor, db_frame):
+    dftmp = db_frame.loc[db_frame['sensor']==sensor]
+    return dftmp
+
+def get_category(row):
+    sensor = row['sensor']
+    for cat in args.cat_list:
+        if sensor in category_dicts.get(cat, set()):
+            return cat
+    return None
+
+def get_sensor(row):
+    directory = row['parent_dir']
+    sensor = directory.split("/")[2]
+    if sensor == 'hirs':
+        sensor = 'hirs/' + directory.split("/")[3]
+    return sensor
+
+def make_sensor_list_by_categories(cat_list):
+    sensor_list = []
+    for category in cat_list:
+        if category in category_dicts:
+            for cat in category_dicts[category]:
+                sensor_list.append("observations/reanalysis/" + cat)
+        else: 
+            print(f"No category found with name {category}")
+    return sensor_list
+
+
+sensor_list = make_sensor_list_by_categories(args.cat_list)
+#read data from sql database of obs counts
+df = utils.get_distinct_bufr_by_sensors(sensor_list)
+
+df['datetime'] = pd.to_datetime(df.obs_day)
+df['sensor'] = df.apply(get_sensor, axis=1)
+df['category'] = df.apply(get_category, axis=1)
+
+df['date_only'] = df['datetime'].dt.date
+
+cris_cutoff = pd.to_datetime("2018-01-16 18:00:00")
+cris_prefix = "observations/reanalysis/cris/cris/"
+df = df.loc[~(df['parent_dir'].str.startswith(cris_prefix) & (df["datetime"] > cris_cutoff))]
+
+# Group by sensor and obs_day-- date only, summing obs_count
+grouped_df = df.groupby(['category', 'date_only'], as_index=False)['obs_count'].sum()
+
+# Convert obs_day to datetime if needed
+grouped_df['date_only'] = pd.to_datetime(grouped_df['date_only'])
+
+# Pivot so categories are columns
+pivot_df = grouped_df.pivot(index='date_only', columns='category', values='obs_count').fillna(0)
+
+# Apply rolling average per category
+pivot_df = pivot_df.rolling(window=args.window, min_periods=1).mean()
+
+# Ensure chronological order
+pivot_df = pivot_df.sort_index()
+
+# Compute stacked cumulative sums across categories
+stacked_df = pivot_df.cumsum(axis=1)
+
+# Create the plot
+fig, ax = plt.subplots(figsize=(10, 8))
+
+# Plot cumulative stacked lines
+for cat in stacked_df.columns:
+    ax.plot(stacked_df.index, stacked_df[cat], label=category_titles.get(cat, cat))
+
+ax.set_title(f'{args.title}', fontsize=18)
+ax.set_xlabel('Observation Day', fontsize=17)
+ax.set_ylabel('Cumulative Average Daily Observation Count', fontsize=17)
+ax.set_yscale('log')  # stacked plot still works with log scale
+ax.set_xlim(daterange)
+
+# Formatting the x-axis for dates (display only the year)
+ax.xaxis.set_major_locator(mdates.YearLocator(5))
+ax.xaxis.set_minor_locator(mdates.YearLocator())
+ax.xaxis.set_major_formatter(mdates.DateFormatter('%Y'))
+plt.xticks(rotation=45, ha='right')
+ax.tick_params(labelsize=12)
+
+# Add grid and legend
+ax.grid(True)
+ax.legend(fontsize=12, loc='upper left')
+
+plt.tight_layout()
+file_name = f"atm_time_series_combo_avg_{args.window}_days.png"
+if args.dev:
+    file_name = f"atm_time_series_combo_avg_{args.window}_days_" + datetime.now().strftime("%Y%m%d%H%M%S") + ".png"
+fnout=os.path.join(args.out_dir,file_name)
+print(f"saving {fnout}")
+plt.savefig(fnout, bbox_inches='tight')
+
+
