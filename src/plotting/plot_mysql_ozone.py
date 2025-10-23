60d3eae3e4f8ef20521f954fdea079a4f666b62b
Jessica Knezha <jessica.knezha@noaa.gov>
2025-10-23 16:56:01 -0600

    inverse file name

--- src/plotting/plot_mysql_ozone.py
+++ src/plotting/plot_mysql_ozone.py
@@ -122,9 +122,14 @@ ax_dup.xaxis.set_minor_locator(mdates.YearLocator(1,month=1,day=1))
 ax_dup.set_xlim(daterange)
 
 plt.suptitle(f'accurate as of {datetime.now().strftime("%m/%d/%Y %H:%M:%S")} UTC', y=-0.01)
-file_name = "ozone_line_observations_inventory_all_sensor.png"
-if args.dev:
-    file_name = "ozone_line_observations_inventory_all_sensor_" + datetime.now().strftime("%Y%m%d%H%M%S") + ".png"
+if args.inverse:
+    file_name = "ozone_line_observations_inventory_all_sensor_inverted.png"
+    if args.dev:
+        file_name = "ozone_line_observations_inventory_all_sensor_inverted_" + datetime.now().strftime("%Y%m%d%H%M%S") + ".png"
+else:
+    file_name = "ozone_line_observations_inventory_all_sensor.png"
+    if args.dev:
+        file_name = "ozone_line_observations_inventory_all_sensor_" + datetime.now().strftime("%Y%m%d%H%M%S") + ".png"
 fnout=os.path.join(args.out_dir,file_name)
 print(f"saving {fnout}")
 plt.savefig(fnout, bbox_inches='tight')
