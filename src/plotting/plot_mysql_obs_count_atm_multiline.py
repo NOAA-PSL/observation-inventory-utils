6c7bd4ccf4cd1538a038d70fbefb0e0fe9f41d7d
Jessica Knezha <jessica.knezha@noaa.gov>
2025-10-29 14:51:11 -0600

    fix all one

--- src/plotting/plot_mysql_obs_count_atm_multiline.py
+++ src/plotting/plot_mysql_obs_count_atm_multiline.py
@@ -102,7 +102,7 @@ def get_category(row):
 def get_variable(row):
     variable = row['variable']
     for cat in args.cat_list:
-        if variable in category_dicts.get(cat, set()):
+        if variable in variable_dicts.get(cat, set()):
             return cat
     return None
 
