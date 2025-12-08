26fa172cc7211e76df136df0ed15d0898ac3a881
Jessica Knezha <jessica.knezha@noaa.gov>
2025-12-08 14:56:57 -0700

    one more try 6

--- src/plotting/plot_mysql_obs_count_conv_sums.py
+++ src/plotting/plot_mysql_obs_count_conv_sums.py
@@ -114,7 +114,7 @@ ax.xaxis.set_major_locator(mdates.YearLocator(5))  # Major ticks every 5 years
 ax.xaxis.set_minor_locator(mdates.YearLocator())  # Minor ticks every year
 ax.xaxis.set_major_formatter(mdates.DateFormatter('%Y'))  # Format major ticks as years
 plt.xticks(rotation=45, ha='right')
-ax.tick_params(labelsize=14)
+ax.tick_params(labelsize=16)
 
 # Add grid and legend
 ax.grid(True)
