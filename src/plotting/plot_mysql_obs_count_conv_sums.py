43047aafb7ae2ef7bc397bda8200286212645124
Jessica Knezha <jessica.knezha@noaa.gov>
2025-12-08 14:30:07 -0700

    increase legend

--- src/plotting/plot_mysql_obs_count_conv_sums.py
+++ src/plotting/plot_mysql_obs_count_conv_sums.py
@@ -118,7 +118,7 @@ ax.tick_params(labelsize=12)
 
 # Add grid and legend
 ax.grid(True)
-ax.legend(fontsize = 14) #11
+ax.legend(fontsize = 16) #11
 
 plt.tight_layout()
 file_name = f"conv_time_series_combo_sum_{args.window}_days.png"
