88e81fc0d89d2ed02960c3c89301d410f851666c
Jessica Knezha <jessica.knezha@noaa.gov>
2025-12-18 14:17:24 -0700

    print for paper values

--- src/plotting/plot_mysql_obs_count_cat_ocn_sums.py
+++ src/plotting/plot_mysql_obs_count_cat_ocn_sums.py
@@ -106,6 +106,8 @@ grouped_df = grouped_df.sort_values(by='date_only')
 
 grouped_df['rolling_avg'] = grouped_df.groupby('variable')['var_count'].transform(lambda x: x.rolling(window=args.window, min_periods=1).mean())
 
+print(grouped_df)
+
 # Get unique variables
 unique_variables = grouped_df['variable'].unique()
 
