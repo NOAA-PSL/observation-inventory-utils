917919914eaee3f0360dcfcec4016f67fa65b9ad
Jessica Knezha <jessica.knezha@noaa.gov>
2026-01-27 15:04:57 -0700

    change names

--- src/plotting/plot_mysql_ocean_ioda.py
+++ src/plotting/plot_mysql_ocean_ioda.py
@@ -96,9 +96,9 @@ ax_dup.xaxis.set_minor_locator(mdates.YearLocator(1,month=1,day=1))
 ax_dup.set_xlim(daterange)
 
 plt.suptitle(f'accurate as of {datetime.now().strftime("%m/%d/%Y %H:%M:%S")} UTC', y=-0.01)
-file_name = "ice_line_observations_inventory_.png"
+file_name = "ocean_ioda_line_observations_inventory_.png"
 if args.dev:
-    file_name = "ice_line_observations_inventory_" + datetime.now().strftime("%Y%m%d%H%M%S") + ".png"
+    file_name = "ocean_ioda_line_observations_inventory_" + datetime.now().strftime("%Y%m%d%H%M%S") + ".png"
 fnout=os.path.join(args.out_dir,file_name)
 print(f"saving {fnout}")
 plt.savefig(fnout, bbox_inches='tight')
